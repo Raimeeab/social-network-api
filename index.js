4e1cbff0c5731117233f108f414f7d361a84217d
Raimeeab <raimee.abbassi@gmail.com>
2022-01-05 15:41:49 +1100

    Debug server starter

--- index.js
+++ index.js
@@ -4,5 +4,19 @@ const routes = require('./routes');
 
 const cwd = process.cwd();
 
+const activity = cwd.includes('social-network-api')
+  ? cwd.split('social-network-api')[1]
+  : cwd;
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
